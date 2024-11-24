7b8e0cfcd5f19842b741fb26bb4f26e360ea04c9
Jandson Vitorino <jandson1512@gmail.com>
2024-11-23 23:07:16 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -47,6 +47,9 @@ export default App;
 // needed to import the default version of the application 
 // needed to import the default version of the application 
 // needed to import the default version of the application 
+// needed to import the default version of the application 
+// needed to import the default version of the application 
+// needed to import the default version of the application 
 
 
 // const App: React.FC = () => {
